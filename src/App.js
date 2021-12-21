641914f7642581dabed4b8f3ea43a751233d0ced
elcinp <elcinpars1@gmail.com>
2021-12-21 14:55:51 +0300

    BrowserRouter added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import MyNav from './components/MyNav';
-import Slider from './components/Slider';
-import Home from './pages/Home';
+import MyNav from "./components/MyNav";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <MyNav/>
-      <Slider/>
-      <Home/>
-      
+      <BrowserRouter>
+        <MyNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Slider from '../components/Slider'
+
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Slider/>
         </div>
     )
 }
