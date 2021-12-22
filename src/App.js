5eaa14b2f9a30e3a687adf9a1ec8cc835691d7fc
elcinp <elcinpars1@gmail.com>
2021-12-22 23:09:13 +0300

    InstructorDeatil.js added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import FullStack from "./pages/FullStack";
 import Instructors from "./pages/Instructor";
+import InstructorDetail from "./pages/InstructorDetail";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/data-science" element={<FullStack />} />
           <Route path="/devops" element={<FullStack />} />
           <Route path="/instructors" element={<Instructors />} />
+          <Route path="/instructors/:login" element={<InstructorDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
