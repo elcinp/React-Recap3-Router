562180264f027a8d724ea629368a494c9dfa4d5a
elcinp <elcinpars1@gmail.com>
2021-12-22 21:58:41 +0300

    courseData mapped

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
+import {Row,Col,Container} from "react-bootstrap"
+import MyCard from "./MyCard"
 
-const Courses = ({data}) => {
-    console.log(data)
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col></Col>
-                </Row>
-            </Container>
-            
-        </div>
-    )
-}
+const Courses = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Container>
+        <Row>
+          {data.map((course) => {
+            return <Col xs={12} md={6} lg={4}>
+                <MyCard/>
+            </Col>;
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default Courses
+export default Courses;
